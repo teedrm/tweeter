58ef611440ae723f9123e577089614851ccba785
Thy Nguyen <tee@Thys-iMac.local>
2022-12-06 23:29:57 -0700

    using jquery to modify the added tweet word counts

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  // word is counted when key is released and when exceeds max number of words allowed, counter will be red and negative
+  $('textarea').keyup(function(e) {
+
+    let charsEntered = $(this).val().length;
+    $('.counter').text(140 - charsEntered);
+
+    if (charsEntered >= 140) {
+      return $('.counter').css("color", "red");
+    }
+    $('.counter').css("color", "#545149");
+  });
+});
